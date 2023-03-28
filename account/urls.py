09b3122b1872ac20196a7ecccf33841bfb9c2086
amosehiguese <amosehiguese@gmail.com>
2023-03-28 19:54:24 +0100

    Add dashboard handler and template

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_view
 from . import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login')
+    # path('login/', views.user_login, name='login')
+    path('login/', auth_view.LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
+
+
+
 ]

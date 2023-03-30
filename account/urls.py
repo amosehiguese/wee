e5d8e80cb27e1b65f5c66a8969d96c5c017f159c
amosehiguese <amosehiguese@gmail.com>
2023-03-30 11:43:20 +0100

    Add password views

--- account/urls.py
+++ account/urls.py
@@ -4,8 +4,16 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+
+    # login and logout urls
     path('login/', auth_view.LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
+
+    # password change urls
+    path('password_change/', auth_view.PasswordChangeView.as_view(template_name='account/password_change_form.html', name='password_change')),
+    path('password_change/done', auth_view.PasswordChangeDoneView.as_view(template_name='password_change_done.html', name='password_change_done')),
+
+
     
 
 

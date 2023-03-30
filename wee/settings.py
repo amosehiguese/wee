7c9e64226fb8f5eeba81d9eabde1cf7c3d6c2514
amosehiguese <amosehiguese@gmail.com>
2023-03-30 13:45:29 +0100

    Modify email configuration

--- wee/settings.py
+++ wee/settings.py
@@ -129,9 +129,11 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 # Email Config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('USER')
 EMAIL_HOST_PASSWORD = os.environ.get('PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 

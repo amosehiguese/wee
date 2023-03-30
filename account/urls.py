27c129ce3241d23614b67e5e1e9f46ed4efcabe4
amosehiguese <amosehiguese@gmail.com>
2023-03-30 12:03:45 +0100

    Add password reset urls

--- account/urls.py
+++ account/urls.py
@@ -10,8 +10,15 @@ urlpatterns = [
     path('logout/', auth_view.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
 
     # password change urls
-    path('password_change/', auth_view.PasswordChangeView.as_view(template_name='account/password_change_form.html', name='password_change')),
-    path('password_change/done', auth_view.PasswordChangeDoneView.as_view(template_name='password_change_done.html', name='password_change_done')),
+    path('password_change/', auth_view.PasswordChangeView.as_view(template_name='account/password_change_form.html'), name='password_change'),
+    path('password_change/done', auth_view.PasswordChangeDoneView.as_view(template_name='account/password_change_done.html'), name='password_change_done'),
+
+    # password reset urls
+    path('password_reset/', auth_view.PasswordResetView.as_view(template_name='account/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_view.PasswordResetDoneView.as_view(template_name='account/password_reset_done.html'), name='password_reset_done' ),
+    path('reset/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(template_name='account/password_reset_email.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_view.PasswordResetCompleteView.as_view(template_name='account/password_reset_complete.html'), name='password_reset_complete')
+
 
 
     

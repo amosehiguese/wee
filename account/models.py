3c7d011f2c65cbafde06f61a2aba70ae9769681a
amosehiguese <amosehiguese@gmail.com>
2023-04-02 14:07:00 +0100

    Add Profile model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+user = get_user_model()
+
+class Profile(models.Model):
+  user = models.OneToOneField(user, on_delete=models.CASCADE)
+  date_of_birth = models.DateField(blank=True, null=True)
+  photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+
+  def __str__(self):
+      return f'Profile for user {self.user.username}'
+  
+
+

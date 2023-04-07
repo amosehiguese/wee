23fb56523c4c7ab19c36b5e221a2f9058db269db
amosehiguese <amosehiguese@gmail.com>
2023-04-08 00:06:33 +0100

    Add image model

--- images/models.py
+++ images/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from account.models import user
+
+class Image(models.Model):
+    user = models.ForeignKey(user, related_name='images_created', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, blank=True)
+    url = models.URLField()
+    image = models.ImageField(upload_to='images/%Y/%m/%d')
+    description = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True, db_index=True)
+
+    def __str__(self):
+      return self.title
